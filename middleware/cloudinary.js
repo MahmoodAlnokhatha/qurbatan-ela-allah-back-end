f66a7dd28f17bf7670d1406edbc9c051c7081866
haddadoz89 <haddadoz1989@gmail.com>
2025-08-07 18:26:47 +0300

    vehicles controllers

---
+++ middleware/cloudinary.js
@@ -0,0 +1,14 @@
+const multer = require('multer')
+const {CloudinaryStorage} = require ('multer-storage-cloudinary')
+const cloudinary = require ('../config/cloudinary')
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'qurbatan-vehicles',
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp', 'heic','heif'],
+    public_id: (req, file) => `${Date.now()}-${file.originalname}`
+  }
+});
+
+module.exports = storage;
